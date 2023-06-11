c0ecfef056b7ada5f304b117e499fb3fc6025df6
coderestore <offby0@gmail.com>
2023-06-11 23:39:01 +0300

    added comments

--- src/main/java/com/company/money/Balance.java
+++ src/main/java/com/company/money/Balance.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Balance {
+    //cumulative balance calculated to date property, including all transaction made in that date
     public int cumulativeAmountToDate;
+    //date until balances are calculated and cached
     public LocalDate date;
+    //monthly balances for UI in form 01/2021 12,00
     public List<String> monthlyBalancesToDate;
 }
